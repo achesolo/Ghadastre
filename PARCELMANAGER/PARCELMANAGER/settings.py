05cd9e22b71c3d774ea62e3e015258da35810939
lord offin <lordoffin30@gmail.com>
2022-12-10 20:27:04 +0000

    Some Updates to the Procfile

--- PARCELMANAGER/PARCELMANAGER/settings.py
+++ PARCELMANAGER/PARCELMANAGER/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-99az87i9c#8q*foxx)uhvaj^5ksgxs_ay%y+n9=+08b!6_bsfy
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['ghadastre.herokuapp.com']
+ALLOWED_HOSTS = ['https://ghadastre.herokuapp.com']
 # ALLOWED_HOSTS = ['*']
 
 
