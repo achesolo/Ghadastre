523f96f6d0db735fb654ca62a8d39813e93f7ffd
lord offin <lordoffin30@gmail.com>
2022-12-10 12:39:04 +0000

    Some Updates to the Procfile

--- PARCELMANAGER/PARCELMANAGER/settings.py
+++ PARCELMANAGER/PARCELMANAGER/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django
-django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +48,6 @@ INSTALLED_APPS = [
     'leaflet',
     'parcels',
     'account',
-    'gunicorn'
 ]
 
 MIDDLEWARE = [
