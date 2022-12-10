eddd7ee1560a43704de1f7f4ee382fc307255e88
lord offin <lordoffin30@gmail.com>
2022-12-10 14:53:55 +0000

    Added white noise

--- PARCELMANAGER/PARCELMANAGER/settings.py
+++ PARCELMANAGER/PARCELMANAGER/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Path where static is stored
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), ]
@@ -154,6 +155,8 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 LEAFLET_CONFIG = {
