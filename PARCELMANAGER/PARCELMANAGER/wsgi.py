6cfa22db87acf1778bf59283b5c34b633eea3c8c
lord offin <lordoffin30@gmail.com>
2022-12-10 12:47:12 +0000

    Some Updates to the Procfile

--- PARCELMANAGER/PARCELMANAGER/wsgi.py
+++ PARCELMANAGER/PARCELMANAGER/wsgi.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
-import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+# import django.core.handlers.wsgi
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PARCELMANAGER.PARCELMANAGER.settings')
 
-# application = get_wsgi_application()
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
+# application = django.core.handlers.wsgi.WSGIHandler()
 
 
 # os.environ["DJANGO_SETTINGS_MODULE"] = "PARCELMANAGER.settings"
