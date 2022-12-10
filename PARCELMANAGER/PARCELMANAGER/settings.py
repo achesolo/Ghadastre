a538bc2e739a74660acd33ad6a9e0fc0c6b3bde8
lord offin <lordoffin30@gmail.com>
2022-12-10 13:59:23 +0000

    Some Updates to the Procfile

--- PARCELMANAGER/PARCELMANAGER/settings.py
+++ PARCELMANAGER/PARCELMANAGER/settings.py
@@ -171,3 +171,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import django_on_heroku
 django_on_heroku.settings(locals())
+
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
