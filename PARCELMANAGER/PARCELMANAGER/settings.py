72e93452c9d062d5129c53fe378dec98c029cbab
lord offin <lordoffin30@gmail.com>
2022-12-10 19:40:06 +0000

    Some Updates to the Procfile

--- PARCELMANAGER/PARCELMANAGER/settings.py
+++ PARCELMANAGER/PARCELMANAGER/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-99az87i9c#8q*foxx)uhvaj^5ksgxs_ay%y+n9=+08b!6_bsfy
 DEBUG = False
 
 # ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['ghadastre.herokuapp.com']
+ALLOWED_HOSTS = ['ghadastre.herokuapp.com']
 # ALLOWED_HOSTS = ['*']
 
 
@@ -178,5 +178,3 @@ import django_on_heroku
 django_on_heroku.settings(locals())
 
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
-
-ALLOWED_HOSTS = ['ghadastre.herokuapp.com']
